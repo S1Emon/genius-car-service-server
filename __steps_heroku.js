8b9c0ae58d3dbbb5abd698d62a702e40ba000ef9
Md Emon Hossain <96044760+S1Emon@users.noreply.github.com>
2022-05-11 13:07:41 +0600

    heroku steps

---
+++ __steps_heroku.js
@@ -0,0 +1,15 @@
+/* **
+---------------------------------
+ONE TIME on your PC
+-------------------------------
+1. create heroku account
+2. verify email
+3. install heroku CLI
+4. heroku login
+-------------------------------------
+For each project one time
+-----------------------------
+1. heroku create
+2. make sure you: git add . git commit & git push
+3. git push heroku main
+*/
